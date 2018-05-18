23874ac2fbff22b01146f00d8c021bdb5129a910
kaarla <sgakarla@ciencias.unam.mx>
2018-05-18 08:40:20 -0500

    50V, Costo:1, 2Q 5-6, 4BxT, Semilla:62

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import(
 
 func main() {
     problema_bombero.TotalBomberos = 10
-    problema_bombero.BomberosXt = 3
+    problema_bombero.BomberosXt = 4
     problema_bombero.HormigasXt = 3
     problema_bombero.Phe = 0.3
     problema_bombero.PheReducion = 0.15
-    problema_bombero.Semilla = 494
+    problema_bombero.Semilla = 62
 
     problema_bombero.CorreHeuristica("grafica50.txt")
 
