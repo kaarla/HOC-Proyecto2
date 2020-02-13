12709bdc62f4b878c516b1cbf850c0518eaed169
kaarla <sgakarla@ciencias.unam.mx>
2020-02-13 06:23:16 -0600

    funciona generar graficas, floyd no :(

--- main.go
+++ main.go
@@ -1,26 +1,39 @@
 package main
 import(
+  "database/sql"
   "fmt"
   "os"
-  "github.com/kaarla/HOC-Proyecto2/util"
   "github.com/kaarla/HOC-Proyecto2/problema_bombero/grafica"
   "github.com/kaarla/HOC-Proyecto2/problema_bombero"
+  _ "github.com/mattn/go-sqlite3"
 )
 
+
+
+func check(e error) {
+    if e != nil {
+        fmt.Println(e)
+        panic(e)
+    }
+}
+
 func main() {
-
-
   if(len(os.Args) <= 1){
     fmt.Println("Uso: \n Para ejecutar ACO sobre el Problema del bombero correr",
      "el comando: \n $ go run main.go problema")
   }else{
     if(os.Args[1] == "grafica"){
-      grafica := grafica.GeneraCuadricula(105)
-      grafica.ImprimeGrafica("graficas/basica30x30.txt")
+      var err error
+      grafica.GraphDB, err = sql.Open("sqlite3", "databases/Grafica3x3.db")
+      check(err)
+      errS := grafica.GeneraBaseCuadricula(4)
+      fmt.Printf("errS: %d ", errS)
+      // grafica := grafica.GeneraCuadricula(25)
+      // grafica.ImprimeGrafica("graficas/basica5x5.txt")
 
-      distancias, recorridos := grafica.FloydWarshal()
-      distancias.ImprimeGrafica("graficas/distancias30x30.txt")
-      recorridos.ImprimeGrafica("graficas/recorridos30x30.txt")
+      // distancias, recorridos := grafica.FloydWarshal()
+      distancias.ImprimeGrafica("graficas/distancias75x75.txt")
+      recorridos.ImprimeGrafica("graficas/recorridos75x75.txt")
 
 
     }else if (os.Args[1] == "problema"){
@@ -28,28 +41,20 @@ func main() {
       grafica := ""
 
       problema_bombero.TotalBomberos = 20
-      problema_bombero.BomberosXt = 5
-      problema_bombero.HormigasXt = 3
+      problema_bombero.BomberosXt = 9
+      problema_bombero.HormigasXt = 6
       problema_bombero.Phe = 0.3
       problema_bombero.PheReducion = 0.15
-      problema_bombero.Semilla =  9355888
-      problema_bombero.PorSalvar = []int{1, 10}
+      problema_bombero.Semilla =  9355
+      problema_bombero.PorSalvar = []int{300, 400, 850, 410}
 
-      grafica = "graficas/distancias10x10.txt"
-      trayectorias := "graficas/recorridos10x10.txt"
+      grafica = "graficas/distancias30x30.txt"
+      trayectorias := "graficas/recorridos30x30.txt"
       problema_bombero.Distancias = problema_bombero.InitMapa(grafica)
       problema_bombero.Trayectorias = problema_bombero.InitMapa(trayectorias)
-      fuegoInicial = []int{36, 66}
+      fuegoInicial = []int{1, 800, 350, 389, 246, 288, 236, 741, 896, 200}
       problema_bombero.CorreHeuristica(grafica, fuegoInicial)
 
-    }else if (os.Args[1] == "arbol"){
-      grafica := grafica.GeneraCuadricula(6)
-      distancias, recorridos := grafica.FloydWarshal()
-      recorridos.ImprimeGrafica("graficas/recorridos9x9.txt")
-
-      arbol := util.CreaArbol(distancias.Nodos, 4, 9)
-      fmt.Println(arbol.Elementos)
-      fmt.Println(arbol.GetTrayectoria(8))
     }
   }
 }
--- problema_bombero/grafica/grafica.go
+++ problema_bombero/grafica/grafica.go
@@ -1,6 +1,8 @@
 package grafica
 
 import (
+  "database/sql"
+  // "errors"
   "fmt"
   "strconv"
   "os"
@@ -8,10 +10,12 @@ import (
 )
 
 var(
-  numVertices = 900
-  numfilas = 30
-  numColumnas = 30
+  numVertices = 9
+  numfilas = 3
+  numColumnas = 3
 )
+
+var GraphDB *sql.DB
 
 /*
   Representacion de la grafica
@@ -20,105 +24,106 @@ type Grafica struct{
   Nodos [][]int
 }
 
+type intNil struct{
+  valid bool
+  value int
+}
+
+
+func GeneraBaseCuadricula(numDiagonales int) int {
+  queryAddColumna := ""
+  queryAddFila := ""
+  var err error
+  for i := 1; i <= numVertices; i++ {
+    queryAddFila = fmt.Sprintf("INSERT INTO grafica (ID) VALUES (%d);", i)
+    _, err = GraphDB.Exec(queryAddFila)
+    check(err)
+    queryAddColumna = fmt.Sprintf("ALTER TABLE grafica ADD `%d` INT;", i)
+    _, err = GraphDB.Exec(queryAddColumna)
+    check(err)
+  }
+  for i := 1; i <= numVertices; i++ {
+    for j := 1; j <= numVertices; j++ {
+      if (j > numColumnas) && (j % numColumnas == 1) {
+        addRelation("grafica", (j - numColumnas), j, 1)
+      } else {
+        if (((j - numColumnas) == i) && (numVertices % j != 1)) || ((j - 1) == i) {
+          addRelation("grafica", i, j, 1)
+        }
+      }
+    }
+  }
+  DiagonalesRandom(numDiagonales)
+  return 1
+}
+
 /*
   Genera la gráfica simple que es un arreglo bidimencional, cuando
   el valor es 1, significa que los vértices que representan los índices
   del arreglo son vecinos.
   Esta es la matriz de distancias.
 */
-func GeneraCuadricula(numDiagonales int) Grafica{
-  var matriz [][]int = make([][]int, numVertices)
-  for k := range matriz{
-    matriz[k] = make([]int, numVertices)
-  }
-  for i := 0; i < len(matriz); i++{
-    for j := 0; j < len(matriz); j++{
-      if((j >= numColumnas) && (j % numColumnas == 0)){
-        matriz[j - numColumnas][j] = 1
-        matriz[j][j - numColumnas] = 1
-      }else{
-        if((j - numColumnas) == i && numVertices % j != 0){
-          matriz[i][j] = 1
-          matriz[j][i] = 1
-        }
-        if((j - 1) == i){
-          matriz[i][j] = 1
-          matriz[j][i] = 1
-        }
-      }
-    }
-  }
-  for i := 0; i < len(matriz); i++{
-    for j := 0; j < len(matriz); j++{
-      if(matriz[i][j] == 0){
-        matriz[i][j] = 2147483647
-        matriz[j][i] = 2147483647
-      }
-    }
-  }
-  grafica := Grafica{}
-  grafica.Nodos = matriz
-  grafica = grafica.DiagonalesRandom(numDiagonales)
-  for i := 0; i < len(matriz); i++{
-    matriz[i][i] = 0
-  }
 
-  // fmt.Println(grafica)
-  return grafica
-}
 
 /*
   Agrega diagonales aleatoriamente a una cuadrícula.
 */
-func (grafica Grafica) DiagonalesRandom(numDiagonales int) Grafica{
-  g := grafica
-  g.Nodos = grafica.Nodos
+func DiagonalesRandom(numDiagonales int){
   i := 0
   j := 0
   c := 1
   for c != numDiagonales{
     i = util.RandInt(0, numVertices - numColumnas)
-    if((i % numColumnas) == (numColumnas - 1)){
+    if((i % numColumnas) == 0){
       j = i
     }else{
       j = i + numColumnas + 1
     }
-    g.Nodos[i][j] = 1
-    g.Nodos[j][i] = 1
+    addRelation("grafica", i, j, 1)
     c++
   }
-  return g
 }
 
-func (grafica Grafica) FloydWarshal() (Grafica, Grafica){
-  distancias := grafica.Nodos
-  var pats [][]int = make([][]int, numVertices)
-  for k := range pats{
-    pats[k] = make([]int, numVertices)
-  }
-  for i := range pats{
-    for j := range pats{
-      if (distancias[i][j] == 2147483647){
-        pats[i][j] = 0
+func FloydWarshal() (Grafica, Grafica){
+  // queryInicial := ""
+  // distancias := grafica.Nodos
+  // var paths [][]int = make([][]int, numVertices)
+  // for k := range paths{
+  //   paths[k] = make([]int, numVertices)
+  // }
+  for i := 1; i <= numVertices; i++ {
+    for j := 1; j <= numVertices; j++ {
+      result := getValue("grafica", i, j)
+      if (result == 2147483647){
+        addRelation("recorridos", i, j, 0)
+        // paths[i][j] = 0
       }else{
-        pats[i][j] = i
+        addRelation("recorridos", i, j, i)
+        // paths[i][j] = i
       }
     }
   }
-  for k := range pats{
-    for i := range pats{
-      for j := range pats{
-        if(distancias[i][j] > distancias[i][k] + distancias[k][j]){
-          distancias[i][j] = distancias[i][k] + distancias[k][j]
-          pats[i][j] = pats[k][j]
+  for k := 1; k <= numVertices; k++ {
+    for i := 1; i <= numVertices; i++ {
+      for j := 1; j <= numVertices; j++ {
+        distIJ := getValue("grafica", i, j)
+        distIK := getValue("grafica", i, k)
+        distKJ := getValue("grafica", k, j)
+        pathKJ := getValue("recorridos", k, j)
+        // if(distancias[i][j] > distancias[i][k] + distancias[k][j]){
+        if(distIJ > distIK + distKJ) {
+          // distancias[i][j] = distancias[i][k] + distancias[k][j]
+          addRelation("grafica", i, j, distIK + distKJ)
+          // paths[i][j] = paths[k][j]
+          addRelation("recorridos", i, j, pathKJ)
         }
       }
     }
   }
   dist := Grafica{}
-  dist.Nodos = distancias
+  // dist.Nodos = distancias
   recorridos := Grafica{}
-  recorridos.Nodos = pats
+  // recorridos.Nodos = paths
   return dist, recorridos
 }
 
@@ -127,18 +132,18 @@ func (grafica Grafica) FloydWarshal() (Grafica, Grafica){
 */
 func (grafica *Grafica) ImprimeGrafica(nombre string){
   s := ""
-  for i := 0; i < len(grafica.Nodos); i++{
-    for j := 0; j < len(grafica.Nodos); j++{
-      s += strconv.Itoa(grafica.Nodos[i][j]) + ","
-    }
-    s = s[:len(s) - 1]
-    s += "\n"
-  }
-  s = s[:len(s) - 1]
-  f, err := os.Create(nombre)
-  check(err)
-  defer f.Close()
-  n3, err := f.WriteString(s)
+  // for i := 0; i < len(grafica.Nodos); i++{
+  //   for j := 0; j < len(grafica.Nodos); j++{
+  //     s += strconv.Itoa(grafica.Nodos[i][j]) + ","
+  //   }
+  //   s = s[:len(s) - 1]
+  //   s += "\n"
+  // }
+  // s = s[:len(s) - 1]
+  f, _ := os.Create(nombre)
+  // check(err)
+  // defer f.Close()
+  n3, _ := f.WriteString(s)
   fmt.Printf("se escribieron %d bytes en %s\n", n3, nombre)
 }
 
@@ -157,8 +162,35 @@ func (grafica *Grafica) ImprimeV(){
   fmt.Println(s)
 }
 
+func addRelation(name string, i int, j int, dist int){
+  query := fmt.Sprintf("UPDATE %s SET `%d` = %d WHERE ID = %d;", name, i, dist, j)
+  _, err := GraphDB.Exec(query)
+  check(err)
+  query = fmt.Sprintf("UPDATE %s SET `%d` = %d WHERE ID = %d;", name, j, dist, i)
+  _, err = GraphDB.Exec(query)
+  check(err)
+}
+
+func getValue(name string, i int, j int) int {
+  query := fmt.Sprintf("SELECT `%d` FROM %s WHERE ID = %d;", i, name, j)
+  result, err := GraphDB.Query(query)
+  check(err)
+  defer result.Close()
+  result.Next()
+  // var jValue intNil
+  // err = result.Scan(&jValue)
+  check(err)
+  fmt.Printf("result: %s", result)
+  // if jValue == nil{
+    return 2147483647
+  // }
+
+  // return int(jValue)
+}
+
 func check(e error) {
     if e != nil {
-        panic(e)
+      fmt.Println(e)
+      panic(e)
     }
 }
